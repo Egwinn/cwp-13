9412fd05c38e8f1884c48e9603e955986ea9974f
Marina <dubovik.m14@gmail.com>
2017-12-22 00:11:47 +0200

    add links between models and create connection

---
+++ models/index.js
@@ -0,0 +1,30 @@
+const dbOptions = {
+    host: config.host,
+    dialect: config.dialect,
+    define : {
+        timestamps : true,
+        paranoid : true
+    }
+};
+
+module.exports = (Sequelize, config) => {
+    const sequelize = new Sequelize(config.db, config.login, config.password, dbOptions);
+    sequelize.authenticate().then(() => {
+        console.log('Connection to database successful');
+    }).catch((err) => {
+        console.log('Unable to connect to database', err);
+    });
+
+    const Fleet = require('../models/fleets')(Sequelize, sequelize);
+    const Motion = require('../models/motions')(Sequelize, sequelize);
+    const Vehicle = require('../models/vehicles')(Sequelize, sequelize);
+
+    Vehicle.hasMany(Fleet, {foreignKey: 'fleetId', sourceKey: 'id'});
+    Motion.hasMany(Vehicle, {foreignKey: 'vehicleId', sourceKey: 'id'});
+
+    return {
+        Fleet, Motion, Vehicle,
+        sequelize: sequelize,
+        Sequelize: Sequelize,
+    };
+};
